f49e71ad1b695ac07408f03ca033d5ae541aecc1
Kevin20201 <59675025+Kevin20201@users.noreply.github.com>
2020-03-29 01:00:54 -0700

    Update MainActivity.java

--- drivesafe/app/src/main/java/com/andersontsai/drivesafe/MainActivity.java
+++ drivesafe/app/src/main/java/com/andersontsai/drivesafe/MainActivity.java
@@ -54,6 +54,10 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 
+import java.net.*;
+import java.io.*;
+import java.util.regex.*; 
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener {
 
     private AppBarConfiguration mAppBarConfiguration;
@@ -166,6 +170,42 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
 
     }
+    
+        //        New Code
+    public String speedLimit(String name, Double x1, Double x2, Double y1, Double y2) {
+        // Thread.sleep(2000);
+        // Parameter name, x1, y1, x2, y2 (Double)
+        // x1 <= x2
+        try {
+            String replace = name.replaceAll(" ", "%20");
+
+            URL oracle = new URL("https://overpass-api.de/api/interpreter?data=way[name=\"" + replace + "\"](" + x1 + ',' + y1 + ',' + x2 + ',' + y2 + ")[maxspeed];out;");
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(oracle.openStream()));
+
+            String inputLine, substr = "999999";
+
+            while ((inputLine = in.readLine()) != null) {
+                String regex = "<tag k=\"maxspeed\" v=\".* ";
+
+                Pattern pattern = Pattern.compile(regex);
+
+                Matcher matcher = pattern.matcher(inputLine);
+
+                if (matcher.find()) {
+                    substr = inputLine.substring(25, inputLine.lastIndexOf(' '));
+                    break;
+                }
+            }
+            in.close();
+            return substr;
+        }
+        catch (Exception e) {
+
+        }
+    }
+//        End Code
 
     @Override
     public void onProviderDisabled(String provider) {
